5ad944d94e8abee82eb8587c735abf6a41a2a9b8
Yashwanth N S <nsyashwanth05@gmail.com>
2025-11-26 17:09:29 +0530

    Trigger first deploy

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// trigger deploy
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './context/AuthContext';
